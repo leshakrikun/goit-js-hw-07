8dd0d1c34b58e8fa7eb6fa73abd2a15471e9e5af
leshakrikun <lesha.krikun@users.noreply.github.com>
2021-04-24 23:14:17 +0300

    without 8

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,5 +2,9 @@ const validationCss = document.getElementById('validation-input')
 const dataLength = Number(validationCss.getAttribute('data-length'))
 validationCss.addEventListener('blur', (event) => {
     const nameLength = event.currentTarget.value.length
-    nameLength === dataLength ? validationCss.classList.add('valid') : validationCss.classList.add('invalid')   
+    validationCss.classList.add('invalid')
+    if (nameLength === dataLength) {
+        validationCss.classList.add('valid')
+        validationCss.classList.toggle('invalid')
+    }
 })
---
+++ src/js/task-08.js
@@ -0,0 +1,7 @@
+ const divEl = document.getElementById('controls')
+const inputEl = divEl.children
+const createBtn = inputEl[1]
+const quantityDivEl = inputEl[0]
+createBtn.addEventListener('click', () => {
+    const quantityEl = quantityDivEl.value
+}) 
